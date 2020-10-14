9d0d3bf1e84298fae65efc94b243a6c2cd7b8750
Nathan Spotten <spottenn@gmail.com>
2020-10-14 16:44:10 -0600

    added try catch for debugging

--- web/edit-projection.php
+++ web/edit-projection.php
@@ -2,13 +2,20 @@
 session_start();
 ?>
 <?php
-include "connect-db.php";
-$projection = htmlspecialchars($_POST['projection-name']);
-$projection = pg_escape_literal($projection);
-$statement = $db->prepare(
-    "SELECT * FROM proj_entries WHERE projection_id = (SELECT id FROM projections WHERE name = $projection)
-    ORDER BY entry_type DESC");
-$statement->execute();
-$entries = $statement->fetchAll(PDO:: FETCH_ASSOC);
+try {
 
+    include "connect-db.php";
+    $projection = htmlspecialchars($_POST['projection-name']);
+    $projection = pg_escape_literal($projection);
+    $statement = $db->prepare(
+        "SELECT * FROM proj_entries WHERE projection_id = (SELECT id FROM projections WHERE name = $projection)
+    ORDER BY entry_type DESC");
+    echo 'prepared<br/>';
+    $statement->execute();
+    $entries = $statement->fetchAll(PDO:: FETCH_ASSOC);
+
+}
+catch (Exception $e) {
+    echo $e->getMessage() . '<br/>' . $e->getTraceAsString();
+}
 print_r($entries);
