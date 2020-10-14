c846dc93c95a0ec985f7543f59f1ab08f3ec89b7
Nathan Spotten <spottenn@gmail.com>
2020-10-13 20:04:59 -0600

    added db retrieval by book

---
+++ web/team05b.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Financial Projector</title>
+</head>
+<body>
+<h1>Scripture in
+    <?php
+    $book = htmlspecialchars($_GET["book"]);
+    echo $book;
+    ?>
+</h1>
+<form action="team05b.php">
+    <input type="text" name="book">
+    <input type="submit">
+</form>
+<?php
+include "connect-db.php";
+
+foreach ($db->query('SELECT * FROM scriptures WHERE book = ' . $book) as $row)
+{
+    echo "<p><strong>";
+    echo $row['book'] . " " . $row['chapter'] .":" . $row['verse'];
+    echo '</strong> - "';
+    echo $row['content'];
+    echo '"</p>';
+}
