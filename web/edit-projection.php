4c8eb1770b03230ada98c640aee3cda544e45d31
Nathan Spotten <spottenn@gmail.com>
2020-10-14 17:21:31 -0600

    put in functions and nicer printing

--- web/edit-projection.php
+++ web/edit-projection.php
@@ -1,22 +1,73 @@
 <?php
-session_start();
-?>
-<?php
-try {
+require "connect-db.php";
 
-    include "connect-db.php";
-    $projection = htmlspecialchars($_POST['projection-name']);
-//    $projection = pg_escape_literal($projection);
-    $statement = $db->prepare(
-        "SELECT * FROM proj_entries WHERE projection_id = (SELECT id FROM projections WHERE name = :projection)
-    ORDER BY entry_type DESC");
-    echo 'prepared<br/>';
-    $statement->bindValue(':projection', $projection);
-    $statement->execute();
-    $entries = $statement->fetchAll(PDO:: FETCH_ASSOC);
+function getEntriesForOne($db, $projection)
+{
+//    $projection = htmlspecialchars($_POST['projection-name']);
+    try {
+        $statement = $db->prepare(
+            "SELECT name, amount_cents, start_date, end_date, repeats, repeat_frequency 
+                FROM proj_entries WHERE projection_id = (SELECT id FROM projections WHERE name = :projection)
+                ORDER BY entry_type DESC");
+        $statement->bindValue(':projection', $projection);
+        $statement->execute();
+        $entries = $statement->fetchAll(PDO:: FETCH_ASSOC);
 
+    } catch (Exception $e) {
+        echo $e->getMessage() . '<br/>' . $e->getTraceAsString();
+    }
+    return $entries;
 }
-catch (Exception $e) {
-    echo $e->getMessage() . '<br/>' . $e->getTraceAsString();
+
+function getBankAccountsForOne($db, $projection)
+{
+//    $projection = htmlspecialchars($_POST['projection-name']);
+    try {
+        $statement = $db->prepare(
+            "SELECT * FROM bank_accounts WHERE projection_id = (SELECT id FROM projections WHERE name = :projection)");
+        $statement->bindValue(':projection', $projection);
+        $statement->execute();
+        $entries = $statement->fetchAll(PDO:: FETCH_ASSOC);
+
+    } catch (Exception $e) {
+        echo $e->getMessage() . '<br/>' . $e->getTraceAsString();
+    }
 }
-print_r($entries);
+
+function getProjectionList($db)
+{
+    try {
+        $statement = $db->prepare(
+            "SELECT name, created, length FROM projections");
+        $statement->execute();
+        $projectionList = $statement->fetchAll(PDO:: FETCH_ASSOC);
+
+    } catch (Exception $e) {
+        echo $e->getMessage() . '<br/>' . $e->getTraceAsString();
+    }
+    return $projectionList;
+}
+
+function printSqlResults($results)
+{
+    echo "<table><tr>";
+    foreach ($results[0] as $key => $value) {
+        echo "<td>" . $key . "</td>";
+    }
+    echo "</tr>";
+
+    foreach ($results as $row) {
+        echo "<tr>";
+        foreach ($row as $data) {
+            echo "<td>" . $data . "</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+}
+
+$projection = htmlspecialchars($_POST['projection-name']);
+$entries = getEntriesForOne($db, $projection);
+printSqlResults($entries);
+
+//print_r($entries);
