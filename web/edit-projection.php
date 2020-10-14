6465a61da8b87ff40a1815e33b1e63a7e65e7fb4
Nathan Spotten <spottenn@gmail.com>
2020-10-14 16:12:27 -0600

    testing db connection 3

--- web/edit-projection.php
+++ web/edit-projection.php
@@ -4,13 +4,11 @@ session_start();
 <?php
 include "connect-db.php";
 $projection = htmlspecialchars($_POST['projection-name']);
-//
-//$statement = $db->prepare(
-//    "SELECT * FROM proj_entries WHERE projection_id = (SELECT id FROM proj_entries WHERE name = :projection)
-//    ORDER BY entry_type DESC");
 
-$statement = $db->prepare("SELECT * FROM proj_entries");
-$statement->execute();
+$statement = $db->prepare(
+    "SELECT * FROM proj_entries WHERE projection_id = (SELECT id FROM proj_entries WHERE name = :projection)
+    ORDER BY entry_type DESC");
+$statement->execute(array(':projection' => $projection));
 $entries = $statement->fetchAll(PDO:: FETCH_ASSOC);
 
 print_r($entries);
