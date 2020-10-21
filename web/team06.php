9db6e6c9393412ddf48fe9e9aace8e5e0bd4143d
Nathan Spotten <spottenn@gmail.com>
2020-10-21 11:53:11 -0600

    bugfix?

--- web/team06.php
+++ web/team06.php
@@ -11,7 +11,9 @@ insertSrcipture($db);
 </head>
 <body>
 <h1>Insert Scripture</h1>
-<form id="insert-form" action="team06InsertScrip.php" method="post">
+<form id="insert-form" action="team06InsertScrip.php" method="post" onsubmit="function () {return false;} {
+
+}">
     <label for="book">Book: </label><input type="text" name="book"><br>
     <label for="chapter">Chapter: </label><input type="text" name="chapter"><br>
     <label for="verse">Verse: </label><input type="text" name="verse"><br>
